309ac74ffc6f776d7412b23778f1cc6a572ff055
morshedul-antor <antor.morshedul@gmail.com>
2023-06-01 22:11:00 +0600

    feat: message component add

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,80 +1,110 @@
-import React from 'react'
+import { faPaperclip, faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useEffect, useState } from 'react'
+import Pic from '../../assets/chat.png'
 import classes from './Chat.module.css'
 
 export default function Chat() {
+    const [chats, setChats] = useState([])
+    const [message, setMessage] = useState('')
+
+    const id = 2
+    const receiver_id = 1
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://127.0.0.1:8000/api/v1/chats/${id}/${receiver_id}`, {
+                    method: 'GET',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                        // Authorization: `Bearer ${token}`,
+                    },
+                })
+
+                const data = await response.json()
+
+                if (response.ok) {
+                    setChats(data)
+                }
+            } catch {}
+        }
+        fetchData()
+
+        const interval = setInterval(fetchData, 1000)
+        return () => clearInterval(interval)
+    }, [])
+
+    const handleSubmit = async () => {
+        const details = {
+            message: message,
+            sender_id: id,
+            receiver_id: receiver_id,
+        }
+
+        if (message.length > 1) {
+            try {
+                const response = await fetch(`http://127.0.0.1:8000/api/v1/chats/`, {
+                    method: 'POST',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(details),
+                })
+
+                const log = await response.json()
+
+                if (response.ok) {
+                    setMessage('')
+                } else {
+                    alert(log.context + '!')
+                }
+            } catch {
+                alert('Please check your internet connection!')
+            }
+        }
+    }
+
     return (
-        <div className={classes.container}>
-            <div className={classes.chatbox}>
-                <div className={classes.topBar}>
-                    <div className={classes.avatar}>
-                        <p>V</p>
-                    </div>
-                    <div className={classes.name}>Voldemort</div>
-                    <div className={classes.icons}>
-                        {/* <i className={classes.fas fa-phone}></i> */}
-                        {/* <i className={classes.fas fa-video}></i> */}
-                    </div>
-                    <div className={classes.menu}>
-                        <div className={classes.dots}></div>
+        <div className={classes.main}>
+            <div className={classes.chat}>
+                <div className={classes.head}>
+                    <img src={Pic} alt="pic" />
+                    <div>
+                        <p>Tony Stark</p>
+                        <span>10:10 AM</span>
                     </div>
                 </div>
-                <div className={classes.middle}>
-                    <div className={classes.voldemort}>
-                        <div className={classes.incoming}>
-                            <div className={classes.bubble}>Hey, Father's Day is coming up..</div>
-                            <div className={classes.bubble}>What are you getting.. Oh, oops sorry dude.</div>
-                        </div>
-                        <div className={classes.outgoing}>
-                            <div className={`${classes.bubble} ${classes.lower}`}>Nah, it's cool.</div>
-                            <div className={classes.bubble}>
-                                Well you should get your Dad a cologne. Here smell it. Oh wait! ...
-                            </div>
-                        </div>
-                        <div className={classes.typing}>
-                            <div className={classes.bubble}>
-                                <div className={`${classes.ellipsis} ${classes.one}`}></div>
-                                <div className={`${classes.ellipsis} ${classes.two}`}></div>
-                                <div className={`${classes.ellipsis} ${classes.three}`}></div>
-                            </div>
-                        </div>
-                    </div>
+                <div className={classes.body}>
+                    {chats &&
+                        chats.map((chat) => (
+                            <p className={chat.sender_id !== id ? classes.receiver : classes.sender}>{chat.message}</p>
+                        ))}
                 </div>
-                <div className={classes.bottomBar}>
-                    <div className={classes.chat}>
-                        <input type="text" placeholder="Type a message..." />
-                        <button type="submit"> {/* <i className={classes.fas fa-paper-plane}></i> */} </button>
+                <div className={classes.footer}>
+                    <div className={classes.icon}>
+                        <FontAwesomeIcon icon={faPaperclip} />
+                    </div>
+                    <div className={classes.message}>
+                        <input
+                            type="text"
+                            placeholder="type message..."
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                        />
+                    </div>
+                    <div
+                        className={classes.send}
+                        onClick={() => {
+                            handleSubmit()
+                            setMessage('')
+                        }}>
+                        <FontAwesomeIcon icon={faPaperPlane} />
                     </div>
                 </div>
             </div>
-            {/* <div className={classes.messages}></div>
-                <div className={classes.profile}>
-                    <div className={classes.avatar}>
-                        <p>H</p>
-                    </div>
-                    <div className={classes.name2}>
-                        Harry<p className={classes.email}>Harry@potter.com</p>
-                    </div>
-                </div>
-                <ul className={classes.people}>
-                    <li className={classes.person focus}>
-                        <span className={classes.title}>Voldemort </span>
-                        <span className={classes.time}>2:50pm</span>
-                        <br />
-                        <span className={classes.preview}>What are you getting... Oh, oops...</span>
-                    </li>
-                    <li className={classes.person}>
-                        <span className={classes.title}>Ron</span>
-                        <span className={classes.time}>2:25pm</span>
-                        <br />
-                        <span className={classes.preview}>Meet me at Hogsmeade and bring...</span>
-                    </li>
-                    <li className={classes.person}>
-                        <span className={classes.title}>Hermione</span>
-                        <span className={classes.time}>2:12pm</span>
-                        <br />
-                        <span className={classes.preview}>Have you and Ron done your hom...</span>
-                    </li>
-                </ul> */}
         </div>
     )
 }
